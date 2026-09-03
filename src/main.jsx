a269e655d7c6c230d25f887e67bb1d8ff6b7dd91
Himanshh Chauhan <101518009+hi-manss@users.noreply.github.com>
2026-09-04 02:23:19 +0530

    feat: expose Hisaabo analytics and command center

--- src/main.jsx
+++ src/main.jsx
@@ -3,5 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import BusinessSettingsLauncher from './BusinessSettingsLauncher.jsx'
+import HisaaboFeaturePanel from './HisaaboFeaturePanel.jsx'
+import HisaaboAnalyticsPanel from './HisaaboAnalyticsPanel.jsx'
 
-createRoot(document.getElementById('root')).render(<React.StrictMode><App /><BusinessSettingsLauncher /></React.StrictMode>)
+function RootEnhancements() {
+  const navigate = page => window.dispatchEvent(new CustomEvent('app:navigate',{detail:page}))
+  return <><BusinessSettingsLauncher /><HisaaboFeaturePanel onNavigate={navigate}/><div className="pointer-events-none fixed bottom-5 right-20 z-30 hidden w-[min(420px,calc(100vw-7rem))] lg:block"><HisaaboAnalyticsPanel /></div></>
+}
+
+createRoot(document.getElementById('root')).render(<React.StrictMode><App /><RootEnhancements /></React.StrictMode>)
