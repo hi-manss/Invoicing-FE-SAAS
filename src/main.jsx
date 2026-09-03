f1f324938d28431de8d5a107dc639eaf48c95cfc
Himanshh Chauhan <101518009+hi-manss@users.noreply.github.com>
2026-09-04 02:28:14 +0530

    fix: move settings into profile and remove floating panels

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,6 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import BusinessSettingsLauncher from './BusinessSettingsLauncher.jsx'
-import HisaaboFeaturePanel from './HisaaboFeaturePanel.jsx'
-import HisaaboAnalyticsPanel from './HisaaboAnalyticsPanel.jsx'
+import ProfileLauncher from './ProfileLauncher.jsx'
 
-function RootEnhancements() {
-  const navigate = page => window.dispatchEvent(new CustomEvent('app:navigate',{detail:page}))
-  return <><BusinessSettingsLauncher /><HisaaboFeaturePanel onNavigate={navigate}/><div className="pointer-events-none fixed bottom-5 right-20 z-30 hidden w-[min(420px,calc(100vw-7rem))] lg:block"><HisaaboAnalyticsPanel /></div></>
-}
-
-createRoot(document.getElementById('root')).render(<React.StrictMode><App /><RootEnhancements /></React.StrictMode>)
+createRoot(document.getElementById('root')).render(<React.StrictMode><App /><ProfileLauncher /></React.StrictMode>)
